f2a955c568efe1042039740c0b35e434f932c11f
Luis Henrique <luishng@gmail.com>
2024-08-19 13:35:16 -0300

    feat: Add AsyncStorage in App

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,17 @@ import { Box } from '@gluestack-ui/themed';
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes'
 import { useAuth } from '@hooks/useAuth';
+import { Loading } from '@components/Loading';
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
 
   const theme = DefaultTheme;
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="$gray700">
