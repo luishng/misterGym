17412847c5e862faca578a63dd6b6c68d8c95987
Luis Henrique <luishng@gmail.com>
2024-08-19 13:39:48 -0300

    Add user information in hear component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,15 +1,25 @@
 import { HStack, Heading, Text, VStack, Icon } from "@gluestack-ui/themed";
 import { LogOut } from 'lucide-react-native'
 
+import userPhotoDefault from '@assets/userPhotoDefault.png'
+
 import { UserPhoto } from "./UserPhoto";
+import { useAuth } from "@hooks/useAuth";
 
 export function HomeHeader() {
+  const { user } = useAuth()
+
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
-      <UserPhoto w="$16" h="$16" alt="Imagem do usuário" source={{ uri: 'https://github.com/luishng.png' }} />
+      <UserPhoto
+        w="$16"
+        h="$16"
+        alt="Imagem do usuário"
+        source={user.avatar ? { uri: user.avatar } : userPhotoDefault}
+      />
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá,</Text>
-        <Heading color="$gray100" fontSize="$md">Luis Henrique</Heading>
+        <Heading color="$gray100" fontSize="$md">{user.name}</Heading>
       </VStack>
 
       <Icon as={LogOut} color="$gray200" size="xl" />
